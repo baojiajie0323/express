40728cf1603b17a4cb6f10c47b75e3570d379b18
baojiajie0323c <382058502@qq.com>
2016-02-26 21:48:30 +0800

    改BUG

--- dao/userSqlMapping.js
+++ dao/userSqlMapping.js
@@ -3,7 +3,8 @@ var user = {
   update:'update user set name=?, age=? where id=?',
   delete: 'delete from user where id=?',
   queryById: 'select * from user where id=?',
-  queryAll: 'select * from user'
+  queryAll: 'select * from user',
+  queryAllGroup: 'select * from emergency_plangroup'
 };
 
 module.exports = user;
